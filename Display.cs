16fd6537bbe98ce70ea2c9e3e17d417eb6095126
ZwyklyBazyl <jarek.kijaczko@gmail.com>
2020-06-11 16:24:50 +0200

    Initial commit

---
+++ Display.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LEDDisplay
+{
+    public partial class Display : UserControl
+    {
+
+        public List<Pixel> pixels = new List<Pixel>();
+
+        SolidBrush paintBrush = new SolidBrush(Color.Red);
+
+        public Color borderColor = Color.DarkBlue;
+
+        public int actualXClearPos;
+
+        public string text = "LCBLA";
+
+        public int speed;
+
+        public int tickRate;
+
+        public Letters letters = new Letters();
+
+        public Display()
+        {
+            InitializeComponent();
+        }
+
+
+        public void Init()
+        {
+            if (!IsComponentInitable) return;
+
+            SetProperties();
+
+            pixels.Clear();
+
+            panel1.CreateGraphics().Clear(Color.White);
+
+            GetLetters();
+
+            timer1.Enabled = true;
+            timer1.Interval = tickRate;
+        }
+
+        private void SetProperties()
+        {
+            letters.width = Int32.Parse(textBox2.Text);
+            letters.height = Int32.Parse(textBox2.Text);
+
+            letters.xPos = Int32.Parse(textBox4.Text);
+            letters.yPos = Int32.Parse(textBox4.Text);
+
+            panel1.Width = Int32.Parse(textBox6.Text);
+            panel1.Height = Int32.Parse(textBox5.Text);
+
+            text = textBox1.Text.ToUpper();
+
+            if (checkBox1.Checked)
+            {
+                speed = -1;
+            }
+            else
+            {
+                speed = 1;
+            }
+
+            tickRate = Int32.Parse(textBox3.Text);
+        }
+
+        private bool IsComponentInitable()
+        {
+            if (textBox1.Text == "" || textBox2.Text == "" || textBox3.Text == "" || textBox4.Text == "" || textBox5.Text == "" || textBox6.Text == "")
+            {
+                MessageBox.Show("Wypełnij wszystkie komórki", "Error Title", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return false;
+            }
+
+            if (Int32.Parse(textBox2.Text) < 0)
+            {
+                MessageBox.Show("Wielkość musi być nieujemna", "Error Title", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return false;
+            }
+
+            if (Int32.Parse(textBox4.Text) < 2 * Int32.Parse(textBox2.Text))
+            {
+                MessageBox.Show("Odstępy musząbyć conajmniej 2 razy większe od wielkości", "Error Title", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return false;
+            }
+
+            if (Int32.Parse(textBox5.Text) <= 0)
+            {
+                MessageBox.Show("Szerokość wyświetlacza musi być dodatnia", "Error Title", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return false;
+            }
+
+            if (Int32.Parse(textBox6.Text) <= 0)
+            {
+                MessageBox.Show("Wysokość wyświetlacza musi być dodatnia", "Error Title", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return false;
+            }
+
+            return true;
+        }
+
+        private void GetLetters()
+        {
+            int actualXOffset = 0;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == 'A')
+                {
+                    pixels.AddRange(letters.GetA(actualXOffset));
+                }
+
+                if (text[i] == 'B')
+                {
+                    pixels.AddRange(letters.GetB(actualXOffset));
+                }
+
+                if (text[i] == 'C')
+                {
+                    pixels.AddRange(letters.GetC(actualXOffset));
+                }
+
+                if (text[i] == 'L')
+                {
+                    pixels.AddRange(letters.GetL(actualXOffset));
+                }
+
+                actualXOffset += 7 * letters.xPos;
+            }
+        }
+
+        public void Draw(int xTransform)
+        {
+            Graphics graphics = panel1.CreateGraphics();
+
+            ControlPaint.DrawBorder(graphics, panel1.ClientRectangle, borderColor, ButtonBorderStyle.Solid);
+
+            foreach (var p in pixels)
+            {
+                p.xPos += xTransform * letters.xPos;
+                paintBrush.Color = p.col;
+                graphics.FillRectangle(paintBrush, p.xPos, p.yPos, p.width, p.height);
+            }
+
+            graphics.Dispose();
+
+
+            if (speed > 0)
+            {
+                if (pixels[0].xPos > panel1.Width)
+                {
+                    Restart();
+                }
+            }
+            else
+            {
+                if (pixels[pixels.Count - 1].xPos < 0)
+                {
+                    Restart();
+                }
+            }
+
+
+        }
+
+        private void Restart()
+        {
+            int lastXPos = pixels[pixels.Count - 1].xPos;
+            int firstXPos = pixels[0].xPos;
+
+            foreach (var p in pixels)
+            {
+                if (speed > 0)
+                {
+                    p.xPos -= lastXPos;
+                }
+                else
+                {
+                    p.xPos -= firstXPos - panel1.Width;
+                }
+
+
+            }
+        }
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            Draw(speed);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Init();
+        }
+
+        private void checkBox2_Click_1(object sender, EventArgs e)
+        {
+            checkBox1.Checked = !checkBox2.Checked;
+        }
+
+        private void checkBox1_Click_1(object sender, EventArgs e)
+        {
+            checkBox2.Checked = !checkBox1.Checked;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            letters.OpenColorPicker();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenColorPicker();
+        }
+
+        public void OpenColorPicker()
+        {
+            ColorDialog colorPicker = new ColorDialog();
+            // Keeps the user from selecting a custom color.
+            colorPicker.AllowFullOpen = false;
+            // Allows the user to get help. (The default is false.)
+            colorPicker.ShowHelp = true;
+            // Sets the initial color select to the current text color.
+            colorPicker.Color = borderColor;
+
+            // Update the text box color if the user clicks OK 
+            if (colorPicker.ShowDialog() == DialogResult.OK)
+                borderColor = colorPicker.Color;
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+    }
+
+    public class Pixel
+    {
+        public int width;
+        public int height;
+        public int xPos;
+        public int yPos;
+        public Color col;
+
+        public Pixel(int w, int h, int x, int y)
+        {
+            width = w;
+            height = h;
+            xPos = x;
+            yPos = y;
+        }
+    }
+}
---
+++ Letters.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace LEDDisplay
+{
+    public class Letters
+    {
+        public int width = 5;
+        public int height = 5;
+        public int xPos = 10;
+        public int yPos = 10;
+
+        public Color paintColor = Color.Red;
+
+        public List<Pixel> GetA(int xOffset)
+        {
+            List<Pixel> pixels = new List<Pixel>();
+
+            pixels.AddRange(AddSpace(xOffset - xPos));
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    var p = new Pixel(width, height, xOffset + i * xPos, 10 + j * yPos);
+
+                    if ((i == 2 && j == 0) || ((i == 1 || i == 3) && j == 1) || ((i == 1 || i == 2 || i == 3) && j == 2) || ((i == 0 || i == 4) && (j == 3 || j == 4)))
+                    {
+                        p.col = paintColor;
+                    }
+                    else
+                    {
+                        p.col = Color.White;
+                    }
+
+                    pixels.Add(p);
+                }
+
+            }
+
+            pixels.AddRange(AddSpace(xOffset + (5 * xPos)));
+
+            return pixels;
+        }
+
+        public List<Pixel> GetB(int xOffset)
+        {
+            List<Pixel> pixels = new List<Pixel>();
+
+            pixels.AddRange(AddSpace(xOffset - xPos));
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    var p = new Pixel(width, height, xOffset + i * xPos, 10 + j * yPos);
+
+                    if (i == 0 || ((i == 1 || i == 2 || i == 3) && (j == 0 || j == 2 || j == 4)) || ((i == 4 && (j == 1 || j == 3))))
+                    {
+                        p.col = paintColor;
+                    }
+                    else
+                    {
+                        p.col = Color.White;
+                    }
+
+                    pixels.Add(p);
+                }
+
+            }
+
+            pixels.AddRange(AddSpace(xOffset + (5 * xPos)));
+
+            return pixels;
+        }
+
+        public List<Pixel> GetC(int xOffset)
+        {
+            List<Pixel> pixels = new List<Pixel>();
+
+            pixels.AddRange(AddSpace(xOffset - xPos));
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    var p = new Pixel(width, height, xOffset + i * xPos, 10 + j * yPos);
+
+                    if ((i == 0 && (j == 1 || j == 2 || j == 3) || ((i == 1 || i == 2 || i == 3 || i == 4) && (j == 0 || j == 4))))
+                    {
+                        p.col = paintColor;
+                    }
+                    else
+                    {
+                        p.col = Color.White;
+                    }
+
+                    pixels.Add(p);
+                }
+
+            }
+
+            pixels.AddRange(AddSpace(xOffset + (5 * xPos)));
+
+            return pixels;
+        }
+
+        public List<Pixel> GetL(int xOffset)
+        {
+            List<Pixel> pixels = new List<Pixel>();
+
+            pixels.AddRange(AddSpace(xOffset - xPos));
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    var p = new Pixel(width, height, xOffset + i * xPos, 10 + j * yPos);
+
+                    if (i == 0 || j == 4)
+                    {
+                        p.col = paintColor;
+                    }
+                    else
+                    {
+                        p.col = Color.White;
+                    }
+
+                    pixels.Add(p);
+                }
+
+            }
+
+            pixels.AddRange(AddSpace(xOffset + (5 * xPos)));
+
+            return pixels;
+        }
+
+        public List<Pixel> AddSpace(int xPos)
+        {
+            List<Pixel> pixels = new List<Pixel>();
+
+            for (int j = 0; j < 5; j++)
+            {
+                var p = new Pixel(width, height, xPos, 10 + j * yPos);
+
+                p.col = Color.White;
+
+                pixels.Add(p);
+            }
+
+            return pixels;
+        }
+
+        public void OpenColorPicker()
+        {
+            ColorDialog colorPicker = new ColorDialog();
+            // Keeps the user from selecting a custom color.
+            colorPicker.AllowFullOpen = false;
+            // Allows the user to get help. (The default is false.)
+            colorPicker.ShowHelp = true;
+            // Sets the initial color select to the current text color.
+            colorPicker.Color = paintColor;
+
+            // Update the text box color if the user clicks OK 
+            if (colorPicker.ShowDialog() == DialogResult.OK)
+                paintColor = colorPicker.Color;
+        }
+    }
+}
